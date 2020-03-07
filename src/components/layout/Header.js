9cedfb6d6f2e07acbe1b6856fc3bde8b5d98cab4
Chloé Balcer <chloebalcer@macbook-pro-de-chloe-4.home>
2020-03-07 16:18:19 +0100

    css header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,41 +1,59 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'; // added
-import { connect } from 'react-redux'; // added
-import { logout } from '../../actions/auth'; // added
+import { connect } from 'react-redux'; 
+import { logout } from '../../actions/auth'; 
+import '../../css/header.css';
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
 
 class Header extends Component {
     render() {
-        const { user, isAuthenticated } = this.props.auth; // added
+        const { user, isAuthenticated } = this.props.auth; 
 
         // added
         const userLinks = (
-            <div className='right menu'>
-                <div className='ui simple dropdown item'>
-                    {user ? user.username : ''}
-                    <i className='dropdown icon' />
-                    <div className='menu'>
-                        <a onClick={this.props.logout} className='item'>
-                            Logout
-              </a>
-                    </div>
-                </div>
-            </div>
+<div className="Header">
+                <p className="brandName">ALTOSCHOLARSHIP</p>              
+                <ul className="menu-ul">
+                    <li><Link className="link" to='/challenges' >CHALLENGES</Link></li>
+                    <li><Link  className="link" to='/exercises'>EXERCISES</Link></li>
+                    <li><Link  className="link" onClick={this.props.logout} to='/'>LOGOUT</Link></li>
+            </ul>      
+            </div>         
         );
 
         // added
         const guestLinks = (
-            <div className='right menu'>
-                <Link to='/login' className='item'>
-                    Login
-          </Link>
-            </div>
-        );
-
+            <nav class="navbar">
+        <span class="navbar-toggle" id="js-navbar-toggle">
+            <i class="fas fa-bars"></i>
+        </span>
+        <a href="#" class="logo">ALTOSCHOLARSHIP</a>
+        <ul class="main-nav" id="js-menu">
+            <li>
+                <Link to='/login' class="nav-links">SIGN IN</Link>
+            </li>
+            <li>
+                <Link to='/register' class="nav-links">SIGN UP</Link>
+            </li>
+        </ul>
+    </nav>
+            // <div className="Header">
+            //     <p className="brandName">ALTOSCHOLARSHIP</p>              
+            //     <ul className="menu-ul">
+            //         <li><Link className="link" to='/login' >SIGN IN</Link></li>
+            //         <li><Link  className="link" to='/register'>SIGN UP</Link></li>
+            // </ul>      
+            // </div>
+        );     
         // updated
         return (
+            
             <div className='ui inverted menu' style={{ borderRadius: '0' }}>
                 {isAuthenticated ? userLinks : guestLinks}
             </div>
+            
         );
     }
 }
@@ -43,6 +61,7 @@ class Header extends Component {
 const mapStateToProps = state => ({
     auth: state.auth
 });
+
 
 // updated
 export default connect(
